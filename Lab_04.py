6219092a0382bc85d50c1a6ff97219b3b02c4626
Kier <KWDV.Lazaro@gmail.com>
2025-08-18 11:05:38 +1200

    Finish q2

--- Lab_04.py
+++ Lab_04.py
@@ -1,4 +1,5 @@
-# Author: KierW.
+# Question1
+"""# Author: KierW.
 # Date 18th August 2025
 
 # Defining variables
@@ -13,4 +14,21 @@ cupcakes_leftover = num_cupcakes % num_people
 #Display output
 print(f"{num_boxes} boxes of cupcakes contain {num_cupcakes} cupcakes.")
 print("Each person gets 4 cupcakes")
-print(f"There are {cupcakes_leftover} cupcakes left over.")
+print(f"There are {cupcakes_leftover} cupcakes left over.")"""
+
+
+
+# Question 2
+# Author: KierW.
+# Date 18th August 2025
+
+title = "=Pythagoras Calculator="
+print("=" * len(title))
+print(title)
+print("=" * len(title))
+
+side_a = 4.0
+side_b = 3.0
+
+length_H = ((side_a ** 2) + (side_b ** 2)) ** 0.5
+print(f"The length of the hypotenuse is {length_H}")
