58c80a2b06b93a7f6df698e1cfd98474fe4dd82d
Kier <KWDV.Lazaro@gmail.com>
2025-08-18 12:03:43 +1200

    Finish q7

--- Lab_04.py
+++ Lab_04.py
@@ -94,10 +94,75 @@ print(f"You received {random_num} out of {test_total}")"""
 # Author: KierW.
 # Date 18th August 2025
 
-import random
+"""import random
+print("Playing craps:")
+dice_1 = random.randint(1, 6)
+dice_2 = random.randint(1, 6)
+total_dice = dice_1 + dice_2
+print(f"You have rolled a {dice_1} and a {dice_2}")
+print(f"for a total of {total_dice}")"""
+
+
+
+# Question 7
+# Author: KierW.
+# Date 18th August 2025
+
+"""import random
 print("Playing craps:")
 dice_1 = random.randint(1, 6)
 dice_2 = random.randint(1, 6)
 total_dice = dice_1 + dice_2
 print(f"You have rolled a {dice_1} and a {dice_2}")
 print(f"for a total of {total_dice}")
+
+# Roll names
+num_2 = "Snake Eyes"
+num_3 = "Ace Deuce"
+num_4 = "Hard Four"  # when both dice show 2
+num_44 = "Easy Four"
+num_5 = "Fever Five"
+num_6 = "Hard Six" # when both dice show 3
+num_66 = "Easy Six"
+num_7 = "Seven Out"
+num_8 = "Hard Eight" # when both dice show 4
+num_88 = "Easy Eight"
+num_9 = "Nina"
+num_10 = "Hard Ten"  # when both dice show 5
+num_1010 = "Easy Ten"
+num_11 = "Yo-leven"
+num_12 = "Boxcars"
+
+if total_dice == 2:
+    print(f"This is called a {num_2}")
+elif total_dice == 3:
+    print(f"This is called a {num_3}")
+elif total_dice == 5:
+    print(f"This is called a {num_5}")
+elif total_dice == 7:
+    print(f"This is called a {num_7}")
+elif total_dice == 9:
+    print(f"This is called a {num_9}")
+elif total_dice == 11:
+    print(f"This is called a {num_11}n")
+elif total_dice == 12:
+    print(f"This is called a B{num_12}")
+    
+if dice_1 == dice_2:
+    if total_dice == 4:
+        print(f"This is called a {num_4}")
+    elif total_dice == 6:
+        print(f"This is called a {num_6}")
+    elif total_dice == 8:
+        print(f"This is called a {num_8}")
+    elif total_dice == 10:
+        print(f"This is called a {num_10}")
+elif dice_1 != dice_2:
+    if total_dice == 4:
+        print(f"This is called a {num_44}")
+    elif total_dice == 6:
+        print(f"This is called a {num_66}")
+    elif total_dice == 8:
+        print(f"This is called a {num_88}")
+    elif total_dice == 10:
+        print(f"This is called a {num_1010}")"""
